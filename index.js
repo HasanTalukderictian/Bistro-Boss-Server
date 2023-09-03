420b67a6598a8782f5b2f4e4e2218e3836270d86
hasantalukder1234 <hasantalukdercou@gmail.com>
2023-09-03 15:25:35 +0600

    cart item load from server

--- index.js
+++ index.js
@@ -46,7 +46,19 @@ async function run() {
         res.send(result);
     })
     
-    // cart collection 
+    // cart collection apis 
+
+    app.get('/carts', async (req, res) =>{
+      const email = req.query.email;
+      
+      if(!email){
+        res.send([]);
+      }
+      const query = {email:email}
+      const result = await cartCollection.find(query).toArray()
+      res.send(result);
+    });
+
 
     app.post('/carts', async(req, res) =>{
       const item = req.body;
